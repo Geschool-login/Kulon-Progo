ff9180daf0c1f24236bb375da7ae4cccc8ff0894
Risviantoe <risviantoe@gmail.com>
2021-12-23 13:03:02 +0700

    menambah fitur carousel & optimasi css

--- src/layouts/PageLayout/index.js
+++ src/layouts/PageLayout/index.js
@@ -12,7 +12,8 @@ import logo from '../../assets/img/logo.png'; //Logo
 function pageLayout( props ) {
     //EDITABLE
     const deskripsi = "Selamat datang kembali di"
-    const title = "Kulon Progo Binangun Blended Learning"
+    const title1 = "Kulon Progo Binangun"
+    const title2 = "Blended Learning"
     const slogan = "Belajar daring di rumah seharusnya tidak menjadi kesempatan untuk kamu bisa bermalas-malasan. Ingat, saat kamu malas, ada banyak orang yang terus berjuang menyusulmu."
     const copyright = "2021 Kulon Progo Binangun Blended Learning - Didukung oleh Geschool"
 
@@ -23,7 +24,8 @@ function pageLayout( props ) {
                 logo={logo}
                 background={background}
                 deskripsi={deskripsi}
-                title={title}
+                title1={title1}
+                title2={title2}
                 slogan={slogan}
                 appName={props.appName}
             >
